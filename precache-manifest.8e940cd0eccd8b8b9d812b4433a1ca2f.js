91847916ad6f25bd6184a30cccbc6244796628b6
gabriel-santos <gabriel.trevisan.santos@gmail.com>
2019-12-05 13:55:50 -0300

    Updates

---
+++ precache-manifest.8e940cd0eccd8b8b9d812b4433a1ca2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aba989f88dead15325b6a54b2c657e39",
+    "url": "/tarefas/index.html"
+  },
+  {
+    "revision": "43e9777796b7d54d236c",
+    "url": "/tarefas/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "86827c76f1898213e683",
+    "url": "/tarefas/static/js/2.e350238b.chunk.js"
+  },
+  {
+    "revision": "43e9777796b7d54d236c",
+    "url": "/tarefas/static/js/main.8251a014.chunk.js"
+  },
+  {
+    "revision": "632a48bb3efc8cf03c32",
+    "url": "/tarefas/static/js/runtime-main.5b19e9e1.js"
+  }
+]);
